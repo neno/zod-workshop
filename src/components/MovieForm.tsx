52f47eb3dbc61166da16a5b02e0af9c99b8e3fba
Nenad Brcic <neno@neno.ch>
2023-06-21 19:28:11 +0200

    Fix types

--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -1,43 +1,53 @@
-"use client";
+'use client';
 
 import { useForm, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
-import {IMovie} from "@/models/movie";
-import {FC} from "react";
-import Link from "next/link";
-import {TextField} from "@/components/TextField";
-import {useRouter} from "next/navigation";
+import { IMovie } from '@/models/movie';
+import { FC } from 'react';
+import Link from 'next/link';
+import { TextField } from '@/components/TextField';
+import { useRouter } from 'next/navigation';
 
 interface ErrorsType {
   string: {
     required_error: string;
     invalid_type_error: string;
-  },
+  };
   number: {
     required_error: string;
     invalid_type_error: string;
-  },
+  };
   nonnegative: {
     message: string;
-  },
+  };
   url: {
     message: string;
-  }
+  };
 }
-const FormSchema = (errors : ErrorsType) => z.object({
-  title: z.string(errors?.string).nonempty({ message: errors.string.required_error}),
-  tagline: z.string(errors?.string),
-  release_date: z.string(errors?.string),
-  runtime: z.coerce.number(errors?.number).nonnegative(errors.nonnegative).optional(),
-  genres: z.string(errors?.string),
-  overview: z.string(errors?.string),
-  budget: z.coerce.number(errors?.number).nonnegative(errors.nonnegative).optional(),
-  revenue: z.coerce.number(errors?.number).nonnegative(errors.nonnegative).optional(),
-  homepage: z.string(errors?.string).url(errors?.url),
-});
-
-type FormInput = z.infer<typeof FormSchema>;
+const FormSchema = (errors: ErrorsType) =>
+  z.object({
+    title: z
+      .string(errors?.string)
+      .nonempty({ message: errors.string.required_error }),
+    tagline: z.string(errors?.string),
+    release_date: z.string(errors?.string),
+    runtime: z.coerce
+      .number(errors?.number)
+      .nonnegative(errors.nonnegative)
+      .optional(),
+    genres: z.string(errors?.string),
+    overview: z.string(errors?.string),
+    budget: z.coerce
+      .number(errors?.number)
+      .nonnegative(errors.nonnegative)
+      .optional(),
+    revenue: z.coerce
+      .number(errors?.number)
+      .nonnegative(errors.nonnegative)
+      .optional(),
+    homepage: z.string(errors?.string).url(errors?.url),
+  });
 
 interface MovieFormProps {
   movie: IMovie;
@@ -45,71 +55,83 @@ interface MovieFormProps {
   errors: any;
 }
 
-export const MovieForm: FC<MovieFormProps> = ({ movie, translations, errors }) => {
+export const MovieForm: FC<MovieFormProps> = ({
+  movie,
+  translations,
+  errors,
+}) => {
+  const resolver = zodResolver(FormSchema(errors));
+  type ResolvedSchema = typeof resolver;
   const router = useRouter();
-  const methods = useForm<FormInput>({
-    resolver: zodResolver(FormSchema(errors)),
+  const methods = useForm<ResolvedSchema>({
+    resolver,
     defaultValues: {
       ...movie,
     },
   });
   const { handleSubmit } = methods;
 
-  const submit = async (data: FormInput) => {
+  const submit = async (data: ResolvedSchema) => {
     try {
       await fetch(`/api/movies`, {
         method: 'PATCH',
-        body: JSON.stringify({...data, id: movie.id}),
-      })
+        body: JSON.stringify({ ...data, id: movie.id }),
+      });
 
       router.push(`/movies/${movie.id}`);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
-    <FormProvider {...methods} >
-      <form className="w-full max-w-xl" noValidate onSubmit={handleSubmit((data) => submit(data))}>
+    <FormProvider {...methods}>
+      <form
+        className='w-full max-w-xl'
+        noValidate
+        onSubmit={handleSubmit((data) => submit(data))}
+      >
         <fieldset>
-          <legend className="text-3xl font-bold">Movie Details</legend>
-          <ol className="grid grid-cols-6 gap-8 mt-8">
-            <li className="col-span-6">
-              <TextField label={translations.title} name="title" />
+          <legend className='text-3xl font-bold'>Movie Details</legend>
+          <ol className='grid grid-cols-6 gap-8 mt-8'>
+            <li className='col-span-6'>
+              <TextField label={translations.title} name='title' />
             </li>
-            <li className="col-span-6">
-              <TextField label="Tagline" name="tagline" />
+            <li className='col-span-6'>
+              <TextField label='Tagline' name='tagline' />
             </li>
-            <li className="col-span-3">
-              <TextField label="Release Date" type="date" name="release_date" />
+            <li className='col-span-3'>
+              <TextField label='Release Date' type='date' name='release_date' />
             </li>
-            <li className="col-span-3">
-              <TextField label="Runtime" type="number" name="runtime" />
+            <li className='col-span-3'>
+              <TextField label='Runtime' type='number' name='runtime' />
             </li>
-            <li className="col-span-6">
-              <TextField label="Genre" name="genres" />
+            <li className='col-span-6'>
+              <TextField label='Genre' name='genres' />
             </li>
-            <li className="col-span-6">
-              <TextField multiline label="Overview" name="overview" />
+            <li className='col-span-6'>
+              <TextField multiline label='Overview' name='overview' />
             </li>
-            <li className="col-span-3">
-              <TextField label="Budget" type="number" name="budget" />
+            <li className='col-span-3'>
+              <TextField label='Budget' type='number' name='budget' />
             </li>
-            <li className="col-span-3">
-              <TextField label="Revenue" type="number" name="revenue" />
+            <li className='col-span-3'>
+              <TextField label='Revenue' type='number' name='revenue' />
             </li>
-            <li className="col-span-6">
-              <TextField label="Homepage" name="homepage" />
+            <li className='col-span-6'>
+              <TextField label='Homepage' name='homepage' />
             </li>
           </ol>
-          <div className="flex justify-between mt-12">
-            <button className="btn btn-primary" type="submit">Submit</button>
-            <Link className="btn btn-error" href={`/movies/${movie.id}`}>
+          <div className='flex justify-between mt-12'>
+            <button className='btn btn-primary' type='submit'>
+              Submit
+            </button>
+            <Link className='btn btn-error' href={`/movies/${movie.id}`}>
               Cancel
             </Link>
           </div>
         </fieldset>
       </form>
     </FormProvider>
-  )
+  );
 };
